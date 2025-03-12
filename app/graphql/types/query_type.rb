91f61c4d899210f3c6511a6b094626bd32e5feca
Rodrigo Castro <rodrigobetet@gmail.com>
2025-03-12 11:28:40 -0300

    Task get ordering based on completed tasks

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -8,7 +8,7 @@ module Types
     end
 
     def tasks(description: nil, completed: nil)
-      tasks = Task.all.order(id: :desc)
+      tasks = Task.all.order(:completed)
       tasks = tasks.where('description LIKE ?', "%#{description}%") if description
       tasks = tasks.where(completed: completed) unless completed.nil?
       tasks
