cc8c6208a95944c145f0ef78ca21d0d4d447daf7
Rodrigo Castro <rodrigobetet@gmail.com>
2025-03-12 22:21:33 -0300

    Edit cors to allow frontend domain

--- config/application.rb
+++ config/application.rb
@@ -49,5 +49,6 @@ module TodoApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.action_dispatch.cookies_same_site_protection = :none
   end
 end
