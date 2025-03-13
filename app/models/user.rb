b045833e02bd999fd1298c6ac08187906b567521
Rodrigo Betetti <rodrigobetetti@MacBook-Pro.local>
2025-03-12 21:28:10 -0300

    Add docker compose

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_secure_password
+  # has_secure_password
   has_many :tasks
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
